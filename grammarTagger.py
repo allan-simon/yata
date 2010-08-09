51846099712dc94b86fbb1194cf767e369f3c690
Sysko <allan.simon@supinfo.com>
2010-08-09 10:56:06 +0200

    added deletion of non possible rule

--- grammarTagger.py
+++ grammarTagger.py
@@ -1,6 +1,5 @@
 from graph.base import Graph
 from itertools import count
-from uuid import uuid4;
 import sys
 
 
@@ -16,7 +15,7 @@ class GrammarTagger:
 
     def tag(self,tagsList):
         taggingGraph = Graph();
-        taggingDict =[]
+        taggingList =[]
         print();
         print( tagsList);
         i = 0;
@@ -43,23 +42,26 @@ class GrammarTagger:
                     connectorNode.shape = "box";
                     connectorNode.connected = True;
                 
+                #we add the rule Node which current node can be part of
                 newTopNode = taggingGraph.add_node( newTopNodeName , **topNode.data );
                 #only for style
                 newTopNode.color = 'green';
                 newTopNode.shape = "box";
+                newTopNode.rule_id = topNode.name;
                 newTopNode.connected = False;
                 
-                currentEdge = taggingGraph.add_edge(
-                    newConnectorName,
-                    newTopNodeName,
-                    label = edge.order,
-                    order = edge.order,
-                    is_directed=False,
-                );
+
                 # now we had the non-connected terminal of the rule
                 j = 0;
                 for outgoingEdge in topNode.outgoing:
-                    if currentEdge.order == outgoingEdge.order:
+                    if edge.order == outgoingEdge.order:
+                        currentEdge = taggingGraph.add_edge(
+                            newConnectorName,
+                            newTopNodeName,
+                            label = edge.order,
+                            order = edge.order,
+                            is_directed=False,
+                        );
                         j += 1;
                         continue
                     
@@ -87,16 +89,23 @@ class GrammarTagger:
                     newTopNodeName
                 );
                 i += 1;
-            taggingDict.append(connectorsList);
+            taggingList.append(connectorsList);
         self.export_graph(taggingGraph, 1);
-        print(taggingDict);
-        self.validate_rule(taggingGraph, taggingDict);
+        self.validate_rule(taggingGraph, taggingList);
         self.export_graph(taggingGraph, 2);
+        self.fusion_rule(taggingGraph, taggingList);
+        self.export_graph(taggingGraph, 3);
+        self.validate_rule(taggingGraph, taggingList);
+        self.export_graph(taggingGraph, 4);
+        self.remove_non_connected_rule(taggingGraph, taggingList);
+        self.export_graph(taggingGraph, 5);
+        self.remove_impossible_rule(taggingGraph, taggingList);
+        self.export_graph(taggingGraph, 6);
 
 
     def export_graph(self, taggingGraph, step) :
         
-        #print(taggingDict);
+        #print(taggingList);
         from graph.extras import dot
         from subprocess import getstatusoutput
 
@@ -113,14 +122,14 @@ class GrammarTagger:
 
        
 
-    # stage 2 validate rule having all its node connectec
-    def validate_rule(self, taggingGraph, taggingDict):
+    # stage 2 validate rule having all its node connected
+    def validate_rule(self, taggingGraph, taggingList):
         print()
         print()
         print("stage 2");
         print()
         print()
-        for position in taggingDict:
+        for position in taggingList:
             for rule in position:
                 ruleNode = taggingGraph[rule];
                 allConnected = True;
@@ -135,4 +144,144 @@ class GrammarTagger:
                 print(ruleNode);
         return 0;
 
+    # stage 3 fusion same occurence of a rule which different
+    # connected and unconnected atom
+    def fusion_rule(self,taggingGraph, taggingList):
+        print()
+        print()
+        print("stage 3");
+        print()
+        print()
+        i = 0;
+        for position in taggingList:
+            for ruleInstance in position: 
+                lastConnected = 0;
+                ruleNode = taggingGraph[ruleInstance];
+                # if the node already have all its item connected
+                # then we already know no fusion is possible
+                if ruleNode.connected == True:
+                    break;
+                ruleId = ruleNode.rule_id;
+                print(ruleInstance)
+                for atomEdge in ruleNode.incoming:
+                    print(atomEdge);
+                    if atomEdge.start.connected == False:
+                        break;
+                    lastConnected +=1;
+                if lastConnected != 0 :
+                    nextPosition = taggingList[i+1];
+                    for nextRule in nextPosition:
+                        nextRuleNode = taggingGraph[nextRule];
+                        if nextRuleNode.rule_id != ruleId:
+                            break;
+                        print("possible fusion");
+                        for nextAtomEdge in nextRuleNode.incoming:
+                            nextAtomNode = nextAtomEdge.start;
+                            if nextAtomEdge.order < lastConnected:
+                                if nextAtomNode.connected == True:
+                                    break
+                            else:
+                                if nextAtomNode.connected == False:
+                                    break
+                                nextAtomNode.connected = False;
+                                nextAtomNode.color = "blue";
+                                
+                                tempNode = ruleNode.incoming[nextAtomEdge.order].start
+                                tempNode.connected = True;
+                                tempNode.color = "red";
 
+
+
+            i += 1;
+
+    def remove_non_connected_rule(self,taggingGraph, taggingList):
+        print()
+        print()
+        print("stage 4");
+        print()
+        print()
+
+        lenTaggingList = len(taggingList);
+        i = 0;
+        while i < lenTaggingList:
+            position = taggingList[i];
+            nbrRuleInstance = len(position);
+            j = 0;
+            while j < nbrRuleInstance:
+                ruleInstance = position[j];
+                ruleNode = taggingGraph[ruleInstance];
+                if ruleNode.connected:
+                    j +=1
+                    continue;
+                #first removed rule which don't have any node connected
+                #anymore
+                noneConnected = True;
+                for edge in ruleNode.incoming:
+                    atomNode = edge.start;
+                    if atomNode.connected:
+                        noneConnected = False;
+                        break;
+                if  noneConnected :
+                    for edge in ruleNode.incoming:
+                        atomNode = edge.start;
+                        taggingGraph.remove_edge(edge);
+                        taggingGraph.remove_node(atomNode);
+                    taggingGraph.remove_node(ruleNode);
+                    position.remove(ruleInstance);
+                    j -= 1;
+                    nbrRuleInstance -= 1;
+                    if not position:
+                        taggingList.remove(position);
+                        lenTaggingList -=1;
+                        i -= 1;
+                j += 1;
+            i +=1
+
+    def remove_impossible_rule(self,taggingGraph, taggingList):
+        print()
+        print()
+        print("stage 5");
+        print()
+        print()
+        print(taggingList)
+        
+        lenTaggingList = len(taggingList);
+        i = 0;
+        while i < lenTaggingList:
+            position = taggingList[i];
+            nbrRuleInstance = len(position);
+            j = 0;
+            while j < nbrRuleInstance:
+                ruleInstance = position[j];
+                ruleNode = taggingGraph[ruleInstance];
+                if ruleNode.connected:
+                    j +=1
+                    continue;
+                print(ruleInstance); 
+                #then removed rule which are looking for an atom which is not present
+                atomNodeFound = False;
+                for edge in ruleNode.incoming:
+                    atomNode = edge.start;
+                    if not atomNode.connected:
+                        if atomNode.category == "atom":
+                            for foudNode in taggingGraph.search_nodes(type = atomNode.type, connected = True):
+                                atomNodeFound = True;
+                                break;
+                            if not atomNodeFound:
+                                break;
+                if not atomNodeFound:
+                    for edge in ruleNode.incoming:
+                        atomNode = edge.start;
+                        taggingGraph.remove_edge(edge);
+                        if not atomNode.connected:
+                            taggingGraph.remove_node(atomNode);
+                    taggingGraph.remove_node(ruleNode);
+                    position.remove(ruleInstance);
+                    j -= 1;
+                    nbrRuleInstance -= 1;
+                    if not position:
+                        taggingList.remove(position);
+                        lenTaggingList -=1;
+                        i -= 1;
+                j += 1;
+            i +=1
